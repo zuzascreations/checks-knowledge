044ef76affc4c906e775688b38b048d2b7ae8888
Zuzana <zu.kucharova@gmail.com>
2021-12-07 12:30:22 +0100

    exercise1-number0to100

--- java-script-dom/1-JS/www/js/script.js
+++ java-script-dom/1-JS/www/js/script.js
@@ -3,7 +3,7 @@
 
 
 const game = (attempts) => {
-    const password = Math.floor(Math.random() * 10)
+    const password = Math.floor(Math.random() * 100)
     console.log(password)
     for (let i = 1; i <= attempts; i++) {
         const insertPassword = prompt('Por favor, introduzca un número entre 0 y 100')
